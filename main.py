23faa5bf81787a17bb4b1b5e07be382b657c5a40
Dr. Tobias Knuth <tknuth@users.noreply.github.com>
2021-05-19 11:47:38 +0200

    close file after writing

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from pathlib import Path
 import json
 
 if __name__ == "__main__":
-    json.dump(analyze_tree(grow_tree()), Path("./data.json").open("w"))
+    with Path("./data.json").open("w") as f:
+        json.dump(analyze_tree(grow_tree()), f)
